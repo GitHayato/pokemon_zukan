f164ac40608a2b0724b80dfeef842d66a7825f0e
Fastman <74091672+GitHayato@users.noreply.github.com>
2022-04-24 15:38:50 +0900

    introduction pokemon

--- lib/main.dart
+++ lib/main.dart
@@ -24,15 +24,32 @@ class MyHomePage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
-      body: Center(
-        child: Text(
-          'Hello World',
-          style: TextStyle(
-            fontSize: 36,
+    return Scaffold(
+        body: Center(
+            child:
+                Column(mainAxisAlignment: MainAxisAlignment.center, children: [
+      Stack(
+        children: [
+          Container(
+            padding: const EdgeInsets.all(32),
+            child: Image.network(
+                "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/25.png",
+                height: 100,
+                width: 100),
           ),
-        ),
+          Container(
+            padding: const EdgeInsets.all(8),
+            child: const Text('No.25',
+                style: TextStyle(fontSize: 16, fontWeight: FontWeight.bold)),
+          ),
+        ],
       ),
-    );
+      const Text('pikachu',
+          style: TextStyle(fontSize: 36, fontWeight: FontWeight.bold)),
+      const Chip(
+        label: Text('electric'),
+        backgroundColor: Colors.yellow,
+      )
+    ])));
   }
 }
